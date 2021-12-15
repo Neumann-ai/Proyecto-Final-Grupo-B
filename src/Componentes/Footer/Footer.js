be03453c811cae09448136da06b43f198fcaef89
Ignacio <ignaciopaliza@gmail.com>
2021-12-15 19:45:24 -0300

    inicio

---
+++ src/Componentes/Footer/Footer.js
@@ -0,0 +1,15 @@
+function Footer (){
+    return(
+        <>
+            <div className="footer">
+                <ul className="redes">
+                    <li><i className=""></i></li>
+                </ul>
+                <a href="top">
+                    <i className=""></i>
+                </a>
+            </div>
+        </>
+    );
+}
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-
+import Footer from "./Componentes/Footer/Footer"
 ReactDOM.render(
   <>
+  <Footer/>
   </>,
   document.getElementById('root')
 );
-
-reportWebVitals();
